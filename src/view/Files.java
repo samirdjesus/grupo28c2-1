43b3bfcff7a620658d3a12366c595792c4705032
SAMIR DIAZ <samirdejesus@gmail.com>
2021-07-26 18:23:24 -0500

    properties files

---
+++ src/view/Files.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+
+public class Files {
+
+   public static void main(String[] args) {
+        try{
+            InputStream ins = new FileInputStream("./data/data.properties");
+            Properties prop = new Properties();
+            prop.load(ins);
+           System.out.println(prop.getProperty("numero67"));
+           //OutputStream ous = new FileOutputStream("./data/data.properties");
+           // Properties prop = new Properties();
+           //for(int i=1;i<=100;i++){
+           //    prop.setProperty("numero"+i, i*3+"");
+          // }
+           //prop.store(ous, null);
+        } catch (Exception e){
+
+        }
+        
+    }
+}
